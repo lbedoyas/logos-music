7efdc7a4f16af132e6a0811949f372a12a66f87f
LB <lucho.bedoya1@hotmail.com>
2021-04-25 19:36:46 -0500

    first commit
    first commit

---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+
+
+  slideOpts = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    centeredSlides: true,
+    speed: 400
+  };
+
+  slides = [{
+    title: 'Escucha tu musica',
+    subTitle: 'EN CUALQUIER LUGAR',
+    description: `Disfruta cada momento de musica,
+    escucha y comparte en cualquier momento`,
+    icon: 'play'
+  },{
+    title: 'Escucha tu postcast',
+    subTitle: 'EN CUALQUIER LUGAR',
+    description: `Disfruta cada momento tu postcast,
+    escucha y comparte en cualquier momento`,
+    icon: 'musical-note'
+  },
+  {
+    title: 'Escucha en tu gym',
+    subTitle: 'EN CUALQUIER LUGAR',
+    description: `Disfruta haciendo ejercicio con logos music`,
+    icon: 'infinite-outline'
+  }
+]
+
+  constructor(private router: Router, private storage: Storage) { }
+
+  ngOnInit() {
+  }
+
+  finish(){
+    this.router.navigateByUrl("/home");
+  }
+
+}
